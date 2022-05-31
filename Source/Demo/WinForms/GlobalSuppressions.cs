cffc92ae20da53af0fef1a7b607c0b4dd3c37289
Chris Seed <chris@thedepartmentofcode.com>
2022-05-31 18:33:47 +0100

    Upgrade HtmlRenderer.WPF and HtmlRenderer.Demo.WPF to .NET 6.

---
+++ Source/Demo/WinForms/GlobalSuppressions.cs
@@ -0,0 +1,23 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Interoperability", "CA1416:Validate platform compatibility", Justification = "<Pending>", Scope = "member", Target = "~F:TheArtOfDev.HtmlRenderer.Demo.WinForms.DemoForm._privateFont")]
+[assembly: SuppressMessage("Interoperability", "CA1416:Validate platform compatibility", Justification = "<Pending>", Scope = "member", Target = "~F:TheArtOfDev.HtmlRenderer.Demo.WinForms.HtmlRenderingHelper._imageCache")]
+[assembly: SuppressMessage("Interoperability", "CA1416:Validate platform compatibility", Justification = "<Pending>", Scope = "member", Target = "~M:TheArtOfDev.HtmlRenderer.Demo.WinForms.DemoForm.GetIcon~System.Drawing.Icon")]
+[assembly: SuppressMessage("Interoperability", "CA1416:Validate platform compatibility", Justification = "<Pending>", Scope = "member", Target = "~M:TheArtOfDev.HtmlRenderer.Demo.WinForms.DemoForm.ImageFromBytes(System.Byte[])~System.Drawing.Image")]
+[assembly: SuppressMessage("Interoperability", "CA1416:Validate platform compatibility", Justification = "<Pending>", Scope = "member", Target = "~M:TheArtOfDev.HtmlRenderer.Demo.WinForms.DemoForm.LoadCustomFonts")]
+[assembly: SuppressMessage("Interoperability", "CA1416:Validate platform compatibility", Justification = "<Pending>", Scope = "member", Target = "~M:TheArtOfDev.HtmlRenderer.Demo.WinForms.GenerateImageForm.#ctor(System.String)")]
+[assembly: SuppressMessage("Interoperability", "CA1416:Validate platform compatibility", Justification = "<Pending>", Scope = "member", Target = "~M:TheArtOfDev.HtmlRenderer.Demo.WinForms.GenerateImageForm.OnPaintBackground(System.Windows.Forms.PaintEventArgs)")]
+[assembly: SuppressMessage("Interoperability", "CA1416:Validate platform compatibility", Justification = "<Pending>", Scope = "member", Target = "~M:TheArtOfDev.HtmlRenderer.Demo.WinForms.GenerateImageForm.OnSaveToFile_Click(System.Object,System.EventArgs)")]
+[assembly: SuppressMessage("Interoperability", "CA1416:Validate platform compatibility", Justification = "<Pending>", Scope = "member", Target = "~M:TheArtOfDev.HtmlRenderer.Demo.WinForms.HtmlRenderingHelper.CreateImageForTransparentBackground~System.Drawing.Bitmap")]
+[assembly: SuppressMessage("Interoperability", "CA1416:Validate platform compatibility", Justification = "<Pending>", Scope = "member", Target = "~M:TheArtOfDev.HtmlRenderer.Demo.WinForms.HtmlRenderingHelper.ImageLoad(TheArtOfDev.HtmlRenderer.Core.Entities.HtmlImageLoadEventArgs,System.Boolean)")]
+[assembly: SuppressMessage("Interoperability", "CA1416:Validate platform compatibility", Justification = "<Pending>", Scope = "member", Target = "~M:TheArtOfDev.HtmlRenderer.Demo.WinForms.HtmlRenderingHelper.TryLoadResourceImage(System.String)~System.Drawing.Image")]
+[assembly: SuppressMessage("Interoperability", "CA1416:Validate platform compatibility", Justification = "<Pending>", Scope = "member", Target = "~M:TheArtOfDev.HtmlRenderer.Demo.WinForms.HtmlRenderingHelper.TryLoadResourceXImage(System.String)~PdfSharpCore.Drawing.XImage")]
+[assembly: SuppressMessage("Interoperability", "CA1416:Validate platform compatibility", Justification = "<Pending>", Scope = "member", Target = "~M:TheArtOfDev.HtmlRenderer.Demo.WinForms.MainControl.#ctor")]
+[assembly: SuppressMessage("Interoperability", "CA1416:Validate platform compatibility", Justification = "<Pending>", Scope = "member", Target = "~M:TheArtOfDev.HtmlRenderer.Demo.WinForms.MainControl.GetFixedHtml~System.String")]
+[assembly: SuppressMessage("Interoperability", "CA1416:Validate platform compatibility", Justification = "<Pending>", Scope = "member", Target = "~M:TheArtOfDev.HtmlRenderer.Demo.WinForms.PerfForm.Run(System.Boolean,System.Boolean)")]
+[assembly: SuppressMessage("Interoperability", "CA1416:Validate platform compatibility", Justification = "<Pending>", Scope = "member", Target = "~M:TheArtOfDev.HtmlRenderer.Demo.WinForms.SampleForm.OnHtmlLabelHostingPanelPaint(System.Object,System.Windows.Forms.PaintEventArgs)")]
